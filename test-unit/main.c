32fbbeb5a976f1498331c493dbf2eef654b78f5b
Denis M <nolane16@gmail.com>
2024-08-18 17:46:30 +0300

    Remove acutest dependency

--- test-unit/main.c
+++ test-unit/main.c
@@ -1,21 +1,20 @@
 #include <libsb.h>
 #include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
 
-static void test_init(void);
-static void test_fini(void);
-#define TEST_INIT test_init()
-#define TEST_FINI test_fini()
-#include <acutest.h>
+static Libsb *libsb;
 
-Libsb *libsb;
+#define ASSERT(x) assert_expression_is_true(x, #x, __FILE__, __LINE__)
+#define E(err) ASSERT(LIBSB_ERROR_OK == (err))
 
-#define E(err) TEST_ASSERT(LIBSB_ERROR_OK == (err))
-
-static void test_init(void) {
-    E(libsb_start(&libsb));
-}
-static void test_fini(void) {
-    E(libsb_finish(&libsb));
+static void assert_expression_is_true(bool value, const char *expression, 
+                                      const char *file, int line) {
+    if (value) {
+        return;
+    }
+    fprintf(stderr, "At \"%s:%d\" assertion is false: \"%s\"", file, line, expression);
+    exit(1);
 }
 
 static void test_empty(void) {
@@ -23,10 +22,10 @@ static void test_empty(void) {
     char *out;
     size_t size;
     E(libsb_create(libsb, &builder));
-    TEST_ASSERT(builder);
+    ASSERT(builder);
     E(libsb_destroy_into(libsb, &builder, &out, &size));
-    TEST_ASSERT(!strcmp(out, ""));
-    TEST_ASSERT(size == strlen(out));
+    ASSERT(!strcmp(out, ""));
+    ASSERT(size == strlen(out));
     free(out);
 }
 
@@ -35,11 +34,11 @@ static void test_append(void) {
     char *out;
     size_t size;
     E(libsb_create(libsb, &builder));
-    TEST_ASSERT(builder);
+    ASSERT(builder);
     E(libsb_append(libsb, builder, "Привет, %s!", "Артём"));
     E(libsb_destroy_into(libsb, &builder, &out, &size));
-    TEST_ASSERT(!strcmp(out, "Привет, Артём!"));
-    TEST_ASSERT(size == strlen(out));
+    ASSERT(!strcmp(out, "Привет, Артём!"));
+    ASSERT(size == strlen(out));
     free(out);
 }
 
@@ -48,23 +47,23 @@ static void test_replace(void) {
     char *out;
     size_t size;
     E(libsb_create(libsb, &builder));
-    TEST_ASSERT(builder);
+    ASSERT(builder);
     E(libsb_append(libsb, builder, "<< one 2 >> << one 2 >>"));
     E(libsb_replace(libsb, builder, "one", "1"));
     E(libsb_replace(libsb, builder, "2", "two"));
     E(libsb_destroy_into(libsb, &builder, &out, &size));
-    TEST_ASSERT(!builder);
-    TEST_ASSERT(!strcmp(out, "<< 1 two >> << 1 two >>"));
-    TEST_ASSERT(size == strlen(out));
+    ASSERT(!builder);
+    ASSERT(!strcmp(out, "<< 1 two >> << 1 two >>"));
+    ASSERT(size == strlen(out));
     free(out);
 
     E(libsb_create(libsb, &builder));
-    TEST_ASSERT(builder);
+    ASSERT(builder);
     E(libsb_append(libsb, builder, "hello"));
     E(libsb_replace(libsb, builder, "l", ""));
     E(libsb_destroy_into(libsb, &builder, &out, &size));
-    TEST_ASSERT(!strcmp(out, "heo"));
-    TEST_ASSERT(size == strlen(out));
+    ASSERT(!strcmp(out, "heo"));
+    ASSERT(size == strlen(out));
     free(out);
 }
 
@@ -73,19 +72,20 @@ static void test_reverse(void) {
     char *out;
     size_t size;
     E(libsb_create(libsb, &builder));
-    TEST_ASSERT(builder);
+    ASSERT(builder);
     E(libsb_append(libsb, builder, "Привет"));
     E(libsb_reverse(libsb, builder));
     E(libsb_destroy_into(libsb, &builder, &out, &size));
-    TEST_ASSERT(!strcmp(out, "тевирП"));
-    TEST_ASSERT(size == strlen(out));
+    ASSERT(!strcmp(out, "тевирП"));
+    ASSERT(size == strlen(out));
     free(out);
 }
 
-TEST_LIST = {
-        { "empty", test_empty },
-        { "append", test_append },
-        { "replace", test_replace },
-        { "reverse", test_reverse },
-        { NULL, NULL },
-};
+int main() {
+    E(libsb_start(&libsb));
+    test_empty();
+    test_append();
+    test_replace();
+    test_reverse();
+    E(libsb_finish(&libsb));
+}
